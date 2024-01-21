075e27d2fa93093aaf077b8cce06a778bf12564b
niicodeer <nicoradin.job@gmail.com>
2024-01-21 01:20:59 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-gradiente1 py-4 px-7">Primary button</button>
+  </div>
+</template>
+<style scoped>
+.btn-gradiente1 {
+  background: rgb(129, 118, 175);
+  background: linear-gradient(
+    90deg,
+    rgba(129, 118, 175, 1) 0%,
+    rgba(192, 183, 232, 1) 100%
+  );
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  },
+  plugins: [vue()],
+})
